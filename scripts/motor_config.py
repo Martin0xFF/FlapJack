89fb2f347b59872493d566868ac0893dc3513d91
Martin <martin.ffrench@mail.utoronto.ca>
2021-04-02 10:56:20 -0400

    improved motor_control_test, uses PID control to set speed to target reference

--- scripts/motor_config.py
+++ scripts/motor_config.py
@@ -1,4 +1,3 @@
-
 import serial
 from collections import deque
 
@@ -90,7 +89,7 @@ class Hermes():
         '''
         when we are done with the serial com, want to free it
         '''
-        if self.ser:
+        if self.get('ser', False):
             self.ser.close()
 
 
@@ -117,29 +116,61 @@ def convert_duty_to_counts(duty):
     '''
     return int((duty - 10000)/462.8 + 14)
 
+def convert_counts_to_duty(counts):
+    '''
+    counts, counts Pos for forward Neg for back
+    '''
+    
+    return abs(int((counts - 14)*462.8 + 10000))
+
 def motor_control(port):
     import time
     aprime = Hermes(port)
-    target_counts_per_sec = 20
-    kpl = 7500
-    kpr = 7500
-    kil = 1
-    kir = 1
+    target_counts_per_sec = 45
+    kpl = 500
+    kil = 200
+    kdl = 15
+      
+    kpr = 500
+    kir = 200
+    kdr = 15
+
+    integral_cutoff = 8000
     eprevl = 0
     eprevr = 0
     elacum = 0
     eracum = 0 
     lefterror=0
     righterror=0
+    aprime.send_command('n')
     while True:
         s = time.time()
+        
         while(True):
+            start = time.time()
             datum = aprime.extract()
-            lefterror = datum['encoderleft'] - 20
-            righterror = datum['encoderright'] - 20
+            delta = time.time() - start
+
+            eprevl = lefterror
+            eprevr = righterror
+            
+            lefterror =  target_counts_per_sec - datum['encoderleft']
+            righterror = target_counts_per_sec - datum['encoderright']
+            
             print(f"{datum['encoderleft']}:{datum['encoderright']}")
-            elacum += lefterror
-            aprime.write_mconfig(1,1, 65535, 10000)
+            elacum += delta*lefterror
+            if elacum > 0:
+                elacum = min(elacum, integral_cutoff)
+            else:
+                elacum = max(elacum, -integral_cutoff)
+            
+            eracum += delta*righterror
+            if eracum > 0:
+                eracum = min(eracum, integral_cutoff)
+            else:
+                eracum = max(eracum, -integral_cutoff)
+
+            aprime.write_mconfig(1,1, int(convert_counts_to_duty(target_counts_per_sec) + kpl*lefterror + kil*elacum + kdl*((eprevl - lefterror)/delta)), int(convert_counts_to_duty(target_counts_per_sec) + kpr*righterror + kir*eracum + kdr*((eprevr - righterror)/delta)))
         s = time.time()
         while(time.time()-s< 3):
             datum = aprime.extract()
@@ -149,6 +180,18 @@ def motor_control(port):
             aprime.write_mconfig(1,1, kpl*lefterror, kpr*righterror)      
 
    
+def mpu_convert(gyro_val, scale=0):
+    '''
+    assume 250 deg/s
+    '''
+    if abs(gyro_val)< 200:
+        gyro_val = 0
+    else:
+        gyro_val = gyro_val - 300
+    return (gyro_val - 32767.5)/131.07 + 250  
+
+
+
 def control_test(port):
     """
     Tilt Test
@@ -160,29 +203,44 @@ def control_test(port):
     Can create angle about wheel using ax and az   
     """
     import numpy as np
-    aprime = Hermes(port, 256000)
-    ref = -0.02
-    kp = 2*65535
-    ki = 0#200
-    kd =0# 65535# 65535//2
+    import time as t
+    
+    aprime = Hermes(port)
+    ref = 0
+    kp = 4000
+    ki = 100
+    kd = 200#200# 65535# 65535//2
+    prev_angle = 0;
+    current_angle = 0;
+    
     prev_error  = 0;
     acum_error = 0;
     error = 0;
+    alpha=0.80
+    aprime.send_command('n')
+    datum = aprime.extract()
+        
+    xoffset = datum['ax']
     while True:
-        theta = 0
-        for i in range(10):
-            datum = aprime.extract()
-            theta += datum['ax']/datum['az']
-        theta = theta/10
+        start = t.time()
+        datum = aprime.extract()
+        delta = t.time() - start 
+        prev_angle = current_angle
+        current_angle =  alpha*(prev_angle + delta*mpu_convert(datum["gy"])) + (1-alpha)*(-np.rad2deg(np.arctan((datum['ax'] - xoffset)/datum['az'])))
+        
         prev_error = error
-        error = theta - ref
+        error = current_angle - ref
         acum_error += error
-        print(f"angle:{error}", end="\r", flush=True)
-        control = round(kp*error + min(ki*(acum_error), 30000) + kd*(error - prev_error)) 
-        if control > 0:
-            aprime.write_mconfig(1,1, control, control);
+        if abs(acum_error) > 1000:
+            acum_error = 1000 if acum_error>0 else -1000 
+        control = int(kp*error + ki*(acum_error) + kd*(error - prev_error)/delta) 
+        sign = control < 0
+        print(f"angle:{error:6.1f}: control:{control:09d}", end="\r", flush=True)
+        control = min(abs(control), 60000)
+        if sign:
+            aprime.write_mconfig(1,1, abs(control), abs(control));
         else:
-            aprime.write_mconfig(0,0, -control, -control);                  
+            aprime.write_mconfig(0,0, abs(control), abs(control));                  
     
     
 
